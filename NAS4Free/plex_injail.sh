da727aad8643fa2a5882ce729f04f21ae291062c
Nostalgist92 <nostalgic.addiction@gmail.com>
2016-02-08 11:10:16 +1000

    Unfinished & Untested Install Scripts

---
+++ NAS4Free/plex_injail.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#	Install script for Plex Media Server in a jailed environment.
+#   See http://forums.nas4free.org/viewtopic.php?f=79&t=8273 or
+#   https://gist.github.com/dreamcat4/f19580cbd31d8f628aca for more info.
+#   Copyrighted 2016 by Ashley Townsend under the Beerware License.

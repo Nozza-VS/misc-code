4f7baf47a4a6164fde47b4587c162d8946e4c30a
Nostalgist92 <nostalgic.addiction@gmail.com>
2016-03-10 13:34:04 +1000

    Move MySQL In Jail With Webmin & phpMyAdmin.sh script to Testing branch

---
+++ NAS4Free/MySQL In Jail With Webmin & phpMyAdmin.sh
@@ -0,0 +1,243 @@
+#!/bin/sh
+# Script Version: 1.0-UNTESTED (March 10, 2016)
+###########################################################################
+#     This is a script to help assist the installation of MySQL,
+#     phpMyAdmin and Webmin in a jailed environment.
+#     Made by Nostalgist92/Nozza. 
+#     Use carefully, I am not responsible for any loss of data.
+###########################################################################
+# This is NOT a FULLY automated script, you WILL have to do some things 
+# to get everything set up successfully
+# Be sure to read carefully!
+###########################################################################
+
+# Add some colour!
+nc='\033[0m'        # No Color
+alt='\033[0;31m'    # Alert Text
+emp='\033[1;31m'    # Emphasis Text
+msg='\033[1;37m'    # Message Text
+url='\033[1;32m'    # URL
+qry='\033[0;36m'    # Query Text
+sep='\033[1;30m-------------------------------------------------------\033[0m'    # Line Seperator
+cmd='\033[1;35m'    # Command to be entered
+fin='\033[0;32m'    # Green Text
+inf='\033[0;33m'    # Information Text
+
+webmin () 
+{
+# Confirm with the user
+read -r -p "   Continue? [Y/n] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+              # If yes, then continue
+              echo -e "${fin} Setting up Webmin..${nc}"
+              pkg install -y webmin
+              echo 'webmin_enable="YES"' >> /etc/rc.conf
+              /usr/local/lib/webmin/setup.sh
+              /usr/local/etc/rc.d/webmin restart
+              echo -e "${msg} You should now be able to visit${nc}"
+              echo -e "${url} http://jailip:10000 ${msg}and log in to webmin.${nc}"
+              echo " "
+               ;;
+    *)
+              # Otherwise exit...
+              echo " "
+              echo -e "${inf} Skipping Webmin..${nc}"
+              echo " "
+              ;;
+esac
+}
+
+phpmyadmin () 
+{
+echo " "
+mkdir /usr/local/www/phpMyAdmin/config && chmod o+w /usr/local/www/phpMyAdmin/config
+chmod o+r /usr/local/www/phpMyAdmin/config.inc.php
+echo -e "${emp} Follow these instructions carefully before continuing:${nc}"
+echo " "
+echo -e "${msg} 1: In your web browser, go to ${url}http://jailip/phpMyAdmin/setup${nc}"
+echo -e "${msg} 2: Click ${cmd}'New server'${msg} and select the ${cmd}'Authentication'${msg} tab.${nc}"
+echo -e "${msg} 3: Under the ${cmd}'Authentication type'${msg} choose ${cmd}'http'${nc}"
+echo -e "${msg}    from the drop-down list (prevents storing login${nc}"
+echo -e "${msg}    credentials directly in config.inc.php)${nc}"
+echo -e "${msg} 4: Also remove ${cmd}'root'${msg} from the ${cmd}'User for config auth'${nc}"
+echo -e "${msg} 5: Now click ${cmd}'Apply'${msg} and you'll return to the Overview page.${nc}"
+echo -e "${msg} 6: Finally, Click ${cmd}'Save'${msg} to save your configuration in${nc}"
+echo -e "${msg}      /usr/local/www/phpMyAdmin/config/config.inc.php.${nc}"
+#Now let’s move that file up one directory to /usr/local/www/phpMyAdmin where phpMyAdmin can make use of it.
+mv /usr/local/www/phpMyAdmin/config/config.inc.php /usr/local/www/phpMyAdmin	
+rm -r /usr/local/www/phpMyAdmin/config
+chmod o-r /usr/local/www/phpMyAdmin/config.inc.php
+echo " "
+read -r -p "   Continue? [Y/n] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+              # If yes, then continue
+              echo -e "${url} Great! Moving on..${nc}"
+              echo " "
+               ;;
+    *)
+              # Otherwise exit...
+              echo " "
+              echo -e "${alt} phpMyAdmin wont work without setting it up.${nc}"
+              echo -e "${msg}    Finishing the rest of the script..${nc}"
+              echo " "
+              ;;
+esac
+}
+
+
+echo " "
+echo -e "${sep}"
+echo -e "${msg}   Welcome to the MySQL guided setup script!${nc}"
+echo -e "${sep}"
+echo " "
+echo " "
+echo " " 
+echo -e "${sep}"
+echo -e "${msg}   Let's start with downloading some packages.${nc}"
+echo -e "${sep}"
+echo " "
+
+pkg install -y mysql56-server mod_php56 php56-mysql php56-mysqli phpmyadmin apache24
+
+# -------------------------------------------------------
+# MySQL
+
+echo " " 
+echo -e "${sep}"
+echo -e "${msg}   Great, now let's get MySQL set up.${nc}"
+echo -e "${sep}"
+echo " "
+
+echo 'mysql_enable="YES"' >> /etc/rc.conf
+echo '[mysqld]' >> /var/db/mysql/my.cnf
+
+/usr/local/etc/rc.d/mysql-server start
+
+mysql_secure_installation
+
+/usr/local/etc/rc.d/mysql-server restart
+
+# -------------------------------------------------------
+# Webmin
+
+echo " " 
+echo -e "${sep}"
+echo -e "${msg}   Would you like Webmin also? (Not required)${nc}"
+echo -e "${sep}"
+echo " "
+
+webmin
+
+# -------------------------------------------------------
+# Apache
+
+echo " " 
+echo -e "${sep}"
+echo -e "${msg}   Getting there! Time for Apache setup.${nc}"
+echo -e "${sep}"
+echo " "
+
+echo 'apache24_enable="YES"' >> /etc/rc.conf
+service apache24 start
+cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
+rehash
+
+# Configure Apache to Use PHP Module
+
+echo '<IfModule dir_module>' >> /usr/local/etc/apache24/Includes/php.conf
+echo '    DirectoryIndex index.php index.html' >> /usr/local/etc/apache24/Includes/php.conf
+echo '    <FilesMatch "\.php$">' >> /usr/local/etc/apache24/Includes/php.conf
+echo '        SetHandler application/x-httpd-php' >> /usr/local/etc/apache24/Includes/php.conf
+echo '    </FilesMatch>' >> /usr/local/etc/apache24/Includes/php.conf
+echo '    <FilesMatch "\.phps$">' >> /usr/local/etc/apache24/Includes/php.conf
+echo '        SetHandler application/x-httpd-php-source' >> /usr/local/etc/apache24/Includes/php.conf
+echo '    </FilesMatch>' >> /usr/local/etc/apache24/Includes/php.conf
+echo '</IfModule>' >> /usr/local/etc/apache24/Includes/php.conf
+
+service apache24 restart
+
+# -------------------------------------------------------
+# Test PHP Processing
+
+# touch /usr/local/www/apache24/data/info.php
+# echo '<?php phpinfo(); ?>' >> /usr/local/www/apache24/data/info.php
+# Add this to file
+# <?php phpinfo(); ?>
+
+# Remove test file
+#rm /usr/local/www/apache24/data/info.php
+
+# -------------------------------------------------------
+# Modify Apache
+
+echo " " 
+echo -e "${sep}"
+echo -e "${msg}   This part needs to be done by you${nc}"
+echo -e "${sep}"
+echo " "
+
+echo -e " Find: DirectoryIndex index.html${nc}"
+echo -e " and add index.php to the end of that line${nc}"
+echo -e " It should then look like this:"
+echo -e "    DirectoryIndex index.html index.php"
+echo -e " Once you're done, press Ctrl+X then Y then Enter"
+
+nano /usr/local/etc/apache24/httpd.conf
+
+# Add this to end of file
+
+echo '<FilesMatch "\.php$">' >> /usr/local/etc/apache24/httpd.conf
+echo '    SetHandler application/x-httpd-php' >> /usr/local/etc/apache24/httpd.conf
+echo '</FilesMatch>' >> /usr/local/etc/apache24/httpd.conf
+echo '<FilesMatch "\.phps$">' >> /usr/local/etc/apache24/httpd.conf
+echo '    SetHandler application/x-httpd-php-source' >> /usr/local/etc/apache24/httpd.conf
+echo '</FilesMatch>' >> /usr/local/etc/apache24/httpd.conf
+echo ' ' >> /usr/local/etc/apache24/httpd.conf
+echo 'Alias /phpMyAdmin "/usr/local/www/phpMyAdmin"' >> /usr/local/etc/apache24/httpd.conf
+echo ' ' >> /usr/local/etc/apache24/httpd.conf
+echo '<Directory "/usr/local/www/phpMyAdmin">' >> /usr/local/etc/apache24/httpd.conf
+echo 'Options None' >> /usr/local/etc/apache24/httpd.conf
+echo 'AllowOverride None' >> /usr/local/etc/apache24/httpd.conf
+echo 'Require all granted' >> /usr/local/etc/apache24/httpd.conf
+echo '</Directory>' >> /usr/local/etc/apache24/httpd.conf
+
+# -------------------------------------------------------
+# phpMyAdmin
+
+echo " " 
+echo -e "${sep}"
+echo -e "${msg}   Now for phpMyAdmin${nc}"
+echo -e "${msg}   This may seem confusing but follow the steps closely${nc}"
+echo -e "${msg}   and you shouldn't run in to any issues!${nc}"
+echo -e "${sep}"
+echo " "
+
+phpmyadmin
+
+# -------------------------------------------------------
+# Now restart Apache, MySQL too for good measure.
+
+echo " " 
+echo -e "${sep}"
+echo -e "${msg}   Last step! Restart apache and mysql.${nc}"
+echo -e "${sep}"
+echo " "
+
+service apache24 restart
+service mysql-server restart
+
+echo " " 
+echo -e "${sep}"
+echo -e "${msg} It looks like we finished here!!! NICE${nc}"
+echo -e "${msg} Now when you have an app that requires a mysql${nc}"
+echo -e "${msg} you can use this jails ip in the host setting${nc}"
+echo " "
+echo " More information will be added to this script later"
+echo " And will also be added to a forum post somewhere."
+echo " "
+echo -e "${msg} You can get in touch with me any of the ways listed here:${nc}"
+echo -e "${url} http://vengefulsyndicate.com/about-us${nc}"
+echo -e "${sep}"
+echo " "

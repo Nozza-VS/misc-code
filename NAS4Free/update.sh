9a97b1e80e399ea6bf438cbe8f1f3aac9cc1a49a
Nostalgist92 <ashleydtownsend@gmail.com>
2017-11-07 15:57:20 +1000

    updated the uhhhh... updater
    not fully tested but should work fine if you cloned this repository
    might look in to changing it to fetch files as they are needed? maybe, see what happens :shrug:

--- NAS4Free/update.sh
+++ NAS4Free/update.sh
@@ -4,9 +4,17 @@
 # By Ashley Townsend (Nozza)    Copyright: Beerware License
 ################################################################################
 
+PKG_OK=$(pkg query %n git-lite|grep "git-lite")
+#Check to see if Git is installed
+if [ "" == "$PKG_OK" ]; then
+  echo "Git not installed, installing now."
+  pkg install -y git-lite
+fi
+
 cd $scriptPath
-echo "$scriptPath"
-cd ../..
-git clone https://github.com/Nostalgist92/misc-code.git
+gitdir="$(dirname "$dir")"
+git pull https://github.com/Nostalgist92/misc-code.git
+
 echo " Done with update, now re-run the 'mainmenu.sh'"
+echo "chmod +x $gitdir"
 exit
